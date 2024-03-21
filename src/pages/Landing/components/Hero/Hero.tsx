2e2a5f5d792212969ab397e39b1d536eac3ee8ea
Bright Kingsley <briggskvngzz@gmail.com>
2024-03-21 07:32:52 +0100

    fix: path to hero background image

--- src/pages/Landing/components/Hero/Hero.tsx
+++ src/pages/Landing/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   const setSearchParams = useSearchParams()[1];
 
   return (
-    <div className="bg-[linear-gradient(to_right,rgb(0,0,0),rgba(0,0,0,0.5)),url('images/landing/banner.webp')] flex-1 min-h-[270px] max-h-[270px] w-full bg-center !bg-cover bg-no-repeat flex flex-col rounded-md">
+    <div className="bg-[linear-gradient(to_right,rgb(0,0,0),rgba(0,0,0,0.5)),url('/public/images/landing/banner.webp')] flex-1 min-h-[270px] max-h-[270px] w-full bg-center !bg-cover bg-no-repeat flex flex-col rounded-md">
       <div className="flex flex-col justify-center flex-grow items-start p-10 gap-3">
         <h1 className="text-5xl font-extrabold uppercase">
           {/* GAME <span className="text-primary">GROW</span> */}

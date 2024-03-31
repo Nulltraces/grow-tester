fc01ab9adc6fa59637c7093ee5ce382afc8b73ee
Bright Kingsley <briggskvngzz@gmail.com>
2024-03-31 03:16:23 +0100

    test: change socket url to http 2

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,11 +3,14 @@ export const BASE_URL = window.location.pathname;
 
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://16.171.134.108"
+    ? // ? "https://16.171.134.108"
+      "https://grow-game-server.onrender.com"
     : "http://localhost:3001";
 
 const socket = io(
-  process.env.NODE_ENV === "production" ? `http://16.171.134.108:3001` : API_URL
+  process.env.NODE_ENV === "production"
+    ? `https://grow-game-server.onrender.com`
+    : API_URL
 );
 
 export default socket;

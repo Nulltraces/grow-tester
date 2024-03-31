905e5c1153dbc3cfbe000fc58df58fe1c119cdc4
Bright Kingsley <briggskvngzz@gmail.com>
2024-03-31 03:00:18 +0100

    test: add port to socket server

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,8 +4,10 @@ export const BASE_URL = window.location.pathname;
 export const API_URL =
   process.env.NODE_ENV === "production"
     ? "https://16.171.134.108"
-    : "http://localhost:3000";
+    : "http://localhost:3001";
 
-const socket = io(API_URL);
+const socket = io(
+  process.env.NODE_ENV === "production" ? `${API_URL}:3001` : API_URL
+);
 
 export default socket;
